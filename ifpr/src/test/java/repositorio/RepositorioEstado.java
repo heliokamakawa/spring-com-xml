fbe3cfef84b3ff2eb1f38427ca41f8e94ea2e37e
heliokamakawa <heliokamakawa@gmail.com>
2019-03-05 11:07:28 -0300

    projeto-conexao

---
+++ ifpr/src/test/java/repositorio/RepositorioEstado.java
@@ -0,0 +1,9 @@
+package repositorio;
+
+import org.springframework.data.repository.CrudRepository;
+
+import entidade.Estado;
+
+public interface RepositorioEstado extends CrudRepository<Estado,Integer>{
+
+}
