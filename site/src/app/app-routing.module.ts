8e3eff31066caf161b602ff8c5e13c90ecab3a35
heliokamakawa <heliokamakawa@gmail.com>
2019-03-20 20:20:10 -0300

    aula02

--- site/src/app/app-routing.module.ts
+++ site/src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { Erro404Component } from './erro404/erro404.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'',component:HomeComponent}
+  ,{path:'home',component:HomeComponent}
+
+  ,{path:'**', component:Erro404Component}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
---
+++ site/src/app/erro404/erro404.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'erro404-component'
+  ,templateUrl: 'erro404.component.html'
+})
+export class Erro404Component{
+}
